49a71c2e4847d154c8023be7f1be22a49ed17dab
Mehrnaz Pishyar <mpishyar.de@gmail.com>
2025-02-11 13:55:17 +0100

    add navbar component & pages

---
+++ src/assets/assets.js
@@ -0,0 +1,12 @@
+import logo from './logo.png'
+import search_icon from './search_icon.png'
+import basket_icon from './basket_icon.png'
+
+
+
+export const assets = {
+
+    logo,
+    search_icon,
+    basket_icon
+}
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+import './Navbar.css'
+import {assets} from '../../assets/assets'
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+    
+  const [menu,setMenu] = useState("home");
+  return (
+    <div className='navbar'>
+        <Link to='/'><img className='logo' src={assets.logo} alt="" /></Link>
+      <ul className="navbar-menu">
+        <Link to="/" onClick={()=>setMenu("home")} className={`${menu==="home"?"active":""}`}>home</Link>
+        <a href='#explore-menu' onClick={()=>setMenu("menu")} className={`${menu==="menu"?"active":""}`}>menu</a>
+        <a href='#app-download' onClick={()=>setMenu("mob-app")} className={`${menu==="mob-app"?"active":""}`}>mobile app</a>
+        <a href='#footer' onClick={()=>setMenu("contact")} className={`${menu==="contact"?"active":""}`}>contact us</a>
+      </ul>
+      <div className="navbar-right">
+        <img src={assets.search_icon} alt="" />
+        <Link to='/cart' className='navbar-search-icon'>
+          <img src={assets.basket_icon} alt="" />
+          <div></div>
+        </Link>
+        <button>sign in</button>
+      </div>
+
+    </div>
+  )
+}
+
+export default Navbar
