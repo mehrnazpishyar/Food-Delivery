f1693c0a608f30b2b683bdb1bdb2126d708cd4fb
Mehrnaz Pishyar <mpishyar.de@gmail.com>
2025-02-11 15:01:16 +0100

    add header section

---
+++ src/components/Navbar/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <div className='header-contents'>
+                <h2>Order your favourite food here!</h2>
+                <p>Explore a diverse menu filled with mouthwatering dishes made from the finest ingredients and prepared with expert craftsmanship. Our goal is to delight your taste buds and make every dining experience unforgettable.</p>
+                <button>View Menu</button>
+            </div>
+        </div>
+    )
+}
+
+export default Header
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import './Home.css'
+import Header from '../../components/Navbar/Header/Header'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+        <Header/>
+    </>
   )
 }
 
