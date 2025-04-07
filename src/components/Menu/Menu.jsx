da6fbb21d4b1607212f7eb7aa0a34eb0f797149a
mehrnaz <pishyar.mehrnaz@gmail.com>
2025-04-07 15:11:26 +0200

    add some responsive styles

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -6,10 +6,10 @@ import { StoreContext } from '../../Context/StoreContext'
 const Menu = ({category,setCategory}) => {
     const {menu_list} = useContext(StoreContext);
   return (
-    <div className='menu' id='menu'>
+    <div className='menu'>
     <h1>Our menu</h1>
     <p className='menu-text'>Explore our diverse menu filled with mouthwatering dishes crafted to delight your taste buds. Our goal is to satisfy your cravings and make every meal a memorable experience.</p>
-    <div className="menu-list">
+    <div className="menu-list" id='menu'>
       {menu_list.map((item,index)=>{
           return (
               <div onClick={()=>setCategory(prev=>prev===item.menu_name?"All":item.menu_name)} key={index} className='menu-list-item'>
